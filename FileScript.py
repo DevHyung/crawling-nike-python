4f2d737078c03c55e95fb3754151ad70271c60e5
devhyung <khuphj@gmail.com>
2018-03-25 02:19:00 +0900

    0325 제작완료

---
+++ FileScript.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+if __name__=="__main__":
+    fileList = os.listdir('./')
+    jpgList = []
+    idx = 0
+    for file in fileList:
+        try:
+            if '(바탕)' in file:
+                shutil.move(os.getcwd()+file,os.getcwd()+file.split('(바탕)')[0]+file)
+        except:
+            print("확인필요 >>> ",file)
+    print(">>> 파일 이동완료 !!!")
+    input("아무키나누르시면 종료됩니다.")

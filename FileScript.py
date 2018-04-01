fe02b6e9f55bcf9419f23f28fb6c602bda4c8f99
devhyung <khuphj@gmail.com>
2018-04-01 21:50:59 +0900

    최종수정

--- FileScript.py
+++ FileScript.py
@@ -9,31 +9,19 @@ if __name__=="__main__":
         if ext.lower() == '.jpg' or ext == '.png':
             try:
                 if '(바탕)' in filename:
-                    shutil.move(dirname + filename, dirname + filename.split('(바탕)')[0] + '/' + filename)
+                    shutil.copyfile(dirname + filename, dirname + filename.split('(바탕)')[0] + '/' + filename)
                 else: #품번만 있는경우
                     folderName = ''
-                    pureName = filename.split('.')[0]
+                    pureName = filename.split('.')[0][:10]
                     for tmp in fileList:
                         if pureName in tmp:
-                            if not filename == tmp:
+                            if not filename == tmp and not '.jpg' in tmp:
                                 folderName = tmp
                                 break
-                    shutil.move(dirname + filename, dirname + folderName + '/' + filename)
+                    print(dirname + filename, dirname + folderName + '/' + filename)
+                    shutil.copyfile(dirname + filename, dirname + folderName + '/' + filename)
             except:
                 print("확인필요 >>> ", filename)
     print(">>> 파일 이동완료 !!!")
     input("아무키나누르시면 종료됩니다.")
     #shutil.move('./샘플파일/846886-222.JPG', './샘플파일/나이키 SB 아이콘 후디 846886-222/')
-    """
-    fileList = os.listdir('./')
-    jpgList = []
-    idx = 0
-    for file in fileList:
-        try:
-            if '(바탕)' in file:
-                shutil.move(os.getcwd()+'\\'+file,os.getcwd()+'\\'+file.split('(바탕)')[0]+'\\'+file)
-        except:
-            print("확인필요 >>> ",file)
-
-
-    """
